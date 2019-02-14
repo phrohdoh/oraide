efba1df12657ef990537641361b62ed5a379ff5c
Taryn Hill <taryn@phrohdoh.com>
2019-02-14 13:31:39 -0600

    Change main.rs to read MiniYaml from a file path instead of inlining test text

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,51 @@
 use std::error::Error;
+use std::str::FromStr;
+use std::env;
+use std::io::Read;
 
 use mltt_span::{Files};
-use unindent::unindent;
+use language_reporting::{
+    ColorArg,
+    termcolor::{
+        StandardStream,
+    },
+};
 
 use oraml2::{
     Lexer,
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    simple_logger::init()?;
+    simple_logger::init_with_level(log::Level::Error)?;
+
+    let file_path = env::args().nth(1).expect("Please provide a file path");
+    let mut f = std::fs::File::open(&file_path).expect("Failed to open provided file path");
+    let mut s = String::new();
+    f.read_to_string(&mut s).expect("Failed to read provided file path");
 
     let mut files = Files::new();
-    let file_id = files.add("test", unindent(r##"
-        ^BasePlayer: # a
-        # b
-            AlwaysVisible: # c
-            # d
-                # e
-                DoTheThing: true # f
-                    # g
-            # h
-    "##));
+    let file_id = files.add(file_path, s);
 
-    let lexer = Lexer::new(&files[file_id]);
-    let tokens = lexer.collect::<Vec<_>>();
+    let mut lexer = Lexer::new(&files[file_id]);
+    let _tokens = lexer.by_ref().collect::<Vec<_>>();
+    let diags = lexer.take_diagnostics();
 
-    let s: String = tokens.iter().map(|tok| tok.slice).collect();
-    print!("{}", s);
+    if diags.is_empty() {
+        println!("Everything is a-ok!");
+    } else {
+        let writer = StandardStream::stdout(ColorArg::from_str("auto").unwrap().into());
+
+        for diag in diags {
+            language_reporting::emit(
+                &mut writer.lock(),
+                &files,
+                &diag,
+                &language_reporting::DefaultConfig,
+            ).unwrap();
+
+            println!();
+        }
+    }
 
     Ok(())
 }
