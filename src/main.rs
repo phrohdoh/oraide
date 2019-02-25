6a3b5bb57e50494c9a9d5b3cf9ffbffa3ec946e2
Taryn Hill <taryn@phrohdoh.com>
2019-02-24 19:01:06 -0600

    Change the bin target to be a crude find-all-refs in a single file

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,84 @@
+// invoke like so:
+// cargo run -- __novcs/medium.yaml ident baseplayer
+
 use std::error::Error;
 use std::str::FromStr;
 use std::env;
 use std::io::Read;
 
-use mltt_span::{Files};
+use mltt_span::{
+    Span,
+    Files,
+    FileId,
+};
+
 use language_reporting::{
+    Label,
+    Diagnostic,
     ColorArg,
     termcolor::{
         StandardStream,
+        StandardStreamLock,
     },
 };
 
 use oraml::{
     Lexer,
     Parser,
+    TokenKind,
 };
+
+fn write_diags<'files>(
+    mut w: &mut StandardStreamLock,
+    files: &'files Files,
+    diags: Vec<Diagnostic<Span<FileId>>>,
+) {
+    for diag in &diags {
+        language_reporting::emit(
+            &mut w,
+            files,
+            diag,
+            &language_reporting::DefaultConfig
+        ).unwrap();
+
+        println!();
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let file_path = env::args().nth(1).expect("Please provide a file path");
+    let mut args = env::args();
+    args.next();
+
+    let file_path = args.next().expect("Please provide a file path");
+    let token_kind: TokenKind = args.next().map(|s|
+        match &s[..] {
+            "ident" | "identifier" => TokenKind::Identifier,
+            "int" => TokenKind::IntLiteral,
+            "float" => TokenKind::FloatLiteral,
+            _ => unimplemented!("Expected token kind {:?} not supported", s),
+        }
+    ).expect("Please provide an expected token kind");
+
+    let search_text = {
+        let text = args.next().expect("Please provide a search value").trim().to_owned();
+
+        if text.is_empty() {
+            panic!("Search text must be non-empty");
+        }
+
+        text
+    };
+
     let mut f = std::fs::File::open(&file_path).expect("Failed to open provided file path");
     let mut s = String::new();
     f.read_to_string(&mut s).expect("Failed to read provided file path");
 
     let mut files = Files::new();
     let file_id = files.add(file_path, s);
+
+    let writer = StandardStream::stdout(ColorArg::from_str("auto").unwrap().into());
 
     // === lexer
 
@@ -34,21 +87,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     let tokens = lexer.by_ref().collect::<Vec<_>>();
     log::debug!("Lexed {} token(s)", tokens.len());
 
+    // === search
+
+    let search_diags = tokens
+        .iter()
+        .filter(|token| token.kind == token_kind)
+        .filter(|token| token.slice.eq_ignore_ascii_case(&search_text))
+        .map(|token|
+            Diagnostic::new_note(format!("Found {:?} {:?}", token_kind, search_text))
+                .with_label(Label::new_primary(token.span))
+        ).collect::<Vec<_>>();
+
+    write_diags(
+        &mut writer.lock(),
+        &files,
+        search_diags
+    );
+
     let lexer_diags = lexer.take_diagnostics();
 
     if !lexer_diags.is_empty() {
-        let writer = StandardStream::stdout(ColorArg::from_str("auto").unwrap().into());
-
-        for diag in &lexer_diags {
-            language_reporting::emit(
-                &mut writer.lock(),
-                &files,
-                diag,
-                &language_reporting::DefaultConfig,
-            ).unwrap();
-
-            println!();
-        }
+        write_diags(
+            &mut writer.lock(),
+            &files,
+            lexer_diags
+        );
     }
 
     // === parser
@@ -59,18 +122,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let parser_diags = parser.take_diagnostics();
     if !parser_diags.is_empty() {
-        let writer = StandardStream::stdout(ColorArg::from_str("auto").unwrap().into());
-
-        for diag in &parser_diags {
-            language_reporting::emit(
-                &mut writer.lock(),
-                &files,
-                diag,
-                &language_reporting::DefaultConfig,
-            ).unwrap();
-
-            println!();
-        }
+        write_diags(
+            &mut writer.lock(),
+            &files,
+            parser_diags
+        );
     }
 
     Ok(())
