f8657764a26605a00390345ab73c8e9d0708a73e
Taryn Hill <taryn@phrohdoh.com>
2019-03-05 10:19:33 -0600

    Change the oraws bin target to print out the ID and Title of each game

--- src/libs/oraws/src/main.rs
+++ src/libs/oraws/src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, fs, io::Read as _};
 use slog::Drain;
+use oraml::TokenCollectionExts as _;
 
 pub mod built_meta {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
@@ -50,7 +51,7 @@ fn run() {
 
         // TODO: Wrap all this up in `oraml`
         let manifest_file_id = files.add(format!("{}", manifest_path_abs.display()), manifest_content);
-        let arena = {
+        let (all_node_ids, arena) = {
             let lexer = oraml::Lexer::new(&files[manifest_file_id]);
             let tokens = lexer.collect::<Vec<_>>();
 
@@ -61,17 +62,35 @@ fn run() {
             arborist.build_tree()
         };
 
-        let metadata_opt_node_ref = arena.iter().find(|&arena_node| {
+        let mut iter = arena.iter().zip(all_node_ids);
+
+        let (_opt_ref_metadata_arena_node, opt_metadata_arena_node_id) = iter.find(|(arena_node, _arena_node_id)| {
             let first_key_token = match arena_node.data.key_tokens.first() {
                 Some(n) => n,
                 _ => return false,
             };
 
             first_key_token.slice == "Metadata"
-        }).map(|arena_node| &arena_node.data);
+        }).map_or((None, None), |(arena_node, arena_node_id)| (Some(&arena_node.data), Some(arena_node_id)));
 
-        if let Some(metadata_node_ref) = metadata_opt_node_ref {
-            log::error!("{}: {:?}", game.id(), metadata_node_ref);
+        if let Some(metadata_arena_node_id) = opt_metadata_arena_node_id {
+            let mut child_ids = metadata_arena_node_id.children(&arena);
+
+            let title_node_opt = child_ids
+                .find(|&id| arena.get(id).map_or(false, |arena_node| arena_node.data.key_tokens.first().map_or(false, |token| token.slice == "Title")))
+                .and_then(|title_node_id| arena.get(title_node_id).map(|arena_node| &arena_node.data));
+
+            if let Some(title_node_ref) = title_node_opt {
+                let value_string = itertools::join(
+                    title_node_ref.value_tokens
+                        .skip_leading_whitespace()
+                        .iter()
+                        .map(|token_ref| token_ref.slice),
+                    ""
+                );
+
+                println!("{}: {}", game.id(), value_string.trim());
+            }
         }
     }
 }
