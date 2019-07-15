671c4e640bbb28ad1b7c5a9e2aac0c9c133a1cfd
Taryn Hill <taryn@phrohdoh.com>
2019-07-15 08:24:58 -0500

    oraide-vscode: General improvements
    - Add a grammar (currently yaml, should be rewritten specifically for miniyaml)
    - Wire up the `oraide.server.exeArgs` config so users can now set that in their VSCode settings
    - Write log files to `<workspace root>/.oraide/logs/` with human-friendly filenames

--- editors/vscode/src/extension.ts
+++ editors/vscode/src/extension.ts
@@ -72,6 +72,12 @@ class ServerConfig {
      * If specified the server will be spawned by executing the file at the given path.
      */
     public get exePath(): string | undefined { return this.config.get<string>('oraide.server.exePath'); }
+
+    /**
+     * If specified the server will be spawned with these arguments
+     */
+    public get exeArgs(): string[] | undefined { return this.config.get<string[]>('oraide.server.exeArgs'); }
+
     public get shouldLogToFile(): boolean { return this.config.get<boolean>('oraide.server.shouldLogToFile', false); }
 }
 
@@ -85,7 +91,7 @@ class ClientWorkspace {
     private readonly dir: WorkspaceFolder;
     private disposables: Disposable[];
 
-    private get dirFsPath() { return this.dir.uri.fsPath; }
+    private get rootDirFsPath() { return this.dir.uri.fsPath; }
 
     constructor(dir: WorkspaceFolder) {
         this.serverConfig = ServerConfig.loadFromWorkspace(dir.uri.fsPath);
@@ -102,19 +108,19 @@ class ClientWorkspace {
                 {
                     language: 'miniyaml',
                     scheme: 'file',
-                    pattern: `${this.dirFsPath}/**/*`,
+                    pattern: `${this.rootDirFsPath}/**/*`,
                 },
                 {
                     language: 'miniyaml',
                     scheme: 'untitled',
-                    pattern: `${this.dirFsPath}/**/*`,
+                    pattern: `${this.rootDirFsPath}/**/*`,
                 },
             ],
             diagnosticCollectionName: 'OpenRA IDE',
             synchronize: { configurationSection: 'oraide' },
             workspaceFolder: this.dir,
             outputChannelName: "OpenRA IDE",
-            revealOutputChannelOn: RevealOutputChannelOn.Info
+            revealOutputChannelOn: RevealOutputChannelOn.Info,
         };
 
         this.client = new LanguageClient(
@@ -132,8 +138,10 @@ class ClientWorkspace {
         // Run the server executable from $PATH, unless there's an override.
         const serverExePath = this.serverConfig.exePath || DEFAULT_SERVER_EXE_NAME;
 
-        const cwd = this.dirFsPath;
-        const serverProcess = spawn(serverExePath, ['ide'], { cwd });
+        const serverExeArgs = this.serverConfig.exeArgs || [ 'ide' ];
+
+        const cwd = this.rootDirFsPath;
+        const serverProcess = spawn(serverExePath, serverExeArgs, { cwd });
 
         serverProcess.on('error', (err: { code?: string; message: string }) => {
             if (err.code === 'ENOENT') {
@@ -143,8 +151,22 @@ class ClientWorkspace {
             }
         });
 
+        const fnFmtDateToHumanFriendlyFileName = (date: Date) => {
+            const year = date.getFullYear().toString();
+            const month = (date.getMonth() + 101).toString().substring(1);
+            const day = (date.getDate() + 100).toString().substring(1);
+            const hour = date.getHours().toString().padStart(2, '0');
+            const minute = date.getMinutes().toString().padStart(2, '0');
+            const second = date.getSeconds().toString().padStart(2, '0');
+            return [year, month, day, hour, minute, second].join('-');
+        };
+
         if (this.serverConfig.shouldLogToFile) {
-            const logPath = path.join(cwd, `oraide-${Date.now()}.log`);
+            const serverStartDateTime = new Date();
+            const formattedDateFileName = fnFmtDateToHumanFriendlyFileName(serverStartDateTime);
+            const logPath = path.join(cwd, `.oraide/logs/${formattedDateFileName}.log`);
+            console.info(`Logging to ${logPath}`);
+
             const logStream = new lazy_stream.Writable(() => fs.createWriteStream(logPath, { flags: 'w+' }));
             serverProcess.stderr.pipe(logStream);
         }
